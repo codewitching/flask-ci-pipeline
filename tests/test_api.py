52a7382b75ca8d5d292d8921b63c7e8412e58dcd
codewitching <anshikaraigarh@gmailcom>
2025-12-19 21:18:40 +0530

    fix test client fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,15 @@
+import pytest 
+from flask import create_app
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+    return app.test_client()
+
 def test_divide_endpoint(client):
     response = client.get("/divide")
     assert response.status_code == 200
     assert response.json["result"] == 5
+
+
